ff335e3a4dd71e2bdd913e78195e6c2d4b293f66
Michael_Vano <michaelv@dev6.com>
2020-05-16 10:40:19 -0400

    fixed font sizing announcement. refine page announcer

--- src/app/core/components/page-announcer/page-announcer.component.ts
+++ src/app/core/components/page-announcer/page-announcer.component.ts
@@ -6,56 +6,62 @@ import {takeWhile} from 'rxjs/operators';
 import {IAnnouncementEvent} from '../../../shared/interfaces/announcement-event';
 
 @Component({
-  selector: 'app-page-announcer',
-  templateUrl: './page-announcer.component.html',
-  styleUrls: ['./page-announcer.component.scss']
+	selector: 'app-page-announcer',
+	templateUrl: './page-announcer.component.html',
+	styleUrls: ['./page-announcer.component.scss']
 })
 export class PageAnnouncerComponent extends BaseComponent implements OnInit{
 
-  announcement: string = 'testing';
-  originalAnnouncement: string;
-  assertiveState: AssertiveStates = AssertiveStates.ASSERTIVE;
-  numberOfRepeats = 1;
+	announcement: string = '';
+	originalAnnouncement: string = '';
+	assertiveState: AssertiveStates = AssertiveStates.ASSERTIVE;
+	numberOfRepeats = 1;
 
-  constructor(private announcerService: AnnouncerService) {
-    super();
-  }
+	constructor(private announcerService: AnnouncerService) {
+		super();
+	}
 
-  ngOnInit() {
-    super.ngOnInit();
-    this.getAnnouncement();
-  }
+	ngOnInit() {
+		super.ngOnInit();
+		this.getAnnouncement();
+	}
 
-  getAnnouncement(): void {
-    this.announcerService.getAnnouncement()
-      .pipe(takeWhile(() => this.active))
-      .subscribe((event: IAnnouncementEvent) => {
-        this.handleAnnouncementEvent(event);
-      });
-  }
+	getAnnouncement(): void {
+		this.announcerService.getAnnouncement()
+			.pipe(takeWhile(() => this.active))
+			.subscribe((event: IAnnouncementEvent) => {
+				this.handleAnnouncementEvent(event);
+			});
+	}
 
-  private handleAnnouncementEvent(event: IAnnouncementEvent): void {
-    if (event.assertiveState !== this.assertiveState) {
-      this.assertiveState = event.assertiveState;
-      setTimeout(() => {
-        this.updateAnnouncementText(event.text);
-      });
-    } else {
-      this.updateAnnouncementText(event.text);
-    }
-  }
+	private handleAnnouncementEvent(event: IAnnouncementEvent): void {
+		setTimeout(() => {
+			if (event.assertiveState !== this.assertiveState) {
+				this.assertiveState = event.assertiveState;
+				setTimeout(() => {
+					this.updateAnnouncementText(event.text);
+				});
+			} else {
+				this.updateAnnouncementText(event.text);
+			}
+		});
+	}
 
-  private updateAnnouncementText(text: string): void {
-    text = text.trim();
-    if (text === this.originalAnnouncement) {
-      this.numberOfRepeats++;
-      const periods = '.' + Array(this.numberOfRepeats).join('.');
-      // adding more periods at the end of the text will cause the screen reader to read out the text again,
-      // without reading the periods out
-      this.announcement = `${text}${periods}`;
-    } else {
-      this.announcement = this.originalAnnouncement = text;
-      this.numberOfRepeats = 0;
-    }
-  }
+	private updateAnnouncementText(text: string): void {
+		text = text.trim();
+		if (text === this.originalAnnouncement) {
+			this.numberOfRepeats++;
+			const periods = '.' + Array(this.numberOfRepeats).join('.');
+			// adding more periods at the end of the text will cause the screen reader to read out the text again,
+			// without reading the periods out
+			this.announcement = `${text}${periods}`;
+		} else {
+			this.announcement = this.originalAnnouncement = text;
+			this.numberOfRepeats = 0;
+		}
+		// clear out announcer so SR do not find it
+		setTimeout(() => {
+			this.announcement = '';
+		}, 250);
+	}
 }
--- src/app/core/services/font-size.service.ts
+++ src/app/core/services/font-size.service.ts
@@ -3,44 +3,41 @@ import {BehaviorSubject, Observable} from 'rxjs';
 import {AnnouncerService} from './announcer.service';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class FontSizeService {
 
-  private _defaultFontSize = 16;
-  private _minFontSize = 10;
-  private _maxFontSize = 32;
-  private _siteBaseFontSize = 16;
-  private _fontSize$: BehaviorSubject<number> = new BehaviorSubject(this._defaultFontSize);
+	private _defaultFontSize = 16;
+	private _minFontSize = 10;
+	private _maxFontSize = 32;
+	private _siteBaseFontSize = 16;
+	private _fontSize$: BehaviorSubject<number> = new BehaviorSubject(this._defaultFontSize);
 
-  constructor(
-  	private announcerService: AnnouncerService,
-  ) { }
+	constructor(
+		private announcerService: AnnouncerService,
+	) { }
 
-  getSiteFontSize(): Observable<number> {
-    return this._fontSize$;
-  }
-
-  resetFontSize(): void {
-    this.updateSiteFontSize(this._defaultFontSize);
-  }
-
-  updateSiteFontSize(size: number): void {
-  	let announcementText = '';
-  	if (size !== this._siteBaseFontSize) {
-		if (size < this._minFontSize) {
-			announcementText = `Smallest text size set at ${this._minFontSize} pixels`;
-			size = this._minFontSize;
-		} else if (size > this._maxFontSize) {
-			announcementText = `Largest text size set at ${this._maxFontSize} pixels`;
-			size = this._maxFontSize;
-		} else {
-			announcementText = `Text size set to ${size} pixels`;
-		}
+	getSiteFontSize(): Observable<number> {
+		return this._fontSize$;
 	}
 
-    this._siteBaseFontSize = size;
-  	this.announcerService.triggerAnnouncement(announcementText);
-    this._fontSize$.next(this._siteBaseFontSize);
-  }
+	resetFontSize(): void {
+		this.updateSiteFontSize(this._defaultFontSize);
+	}
+
+  	updateSiteFontSize(size: number): void {
+		let announcementText = `Text size set to ${size} pixels`;
+		if (size !== this._siteBaseFontSize) {
+			if (size < this._minFontSize) {
+				announcementText = `Smallest text size set at ${this._minFontSize} pixels`;
+				size = this._minFontSize;
+			} else if (size > this._maxFontSize) {
+				announcementText = `Largest text size set at ${this._maxFontSize} pixels`;
+				size = this._maxFontSize;
+			}
+		}
+		this._siteBaseFontSize = size;
+		this.announcerService.triggerAnnouncement(announcementText);
+		this._fontSize$.next(this._siteBaseFontSize);
+  	}
 }
