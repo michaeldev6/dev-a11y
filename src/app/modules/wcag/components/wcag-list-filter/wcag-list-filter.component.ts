6f10607db4efc8ef4ddf5aead0b341325d6633c4
Michael Vano <michaelv@dev6.com>
2020-07-04 16:17:22 -0400

    setup a11y select component

--- src/app/modules/wcag/components/wcag-list-filter/wcag-list-filter.component.ts
+++ src/app/modules/wcag/components/wcag-list-filter/wcag-list-filter.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {BaseComponent} from '../../../../core/components/base/base.component';
+import {ISelectOptions} from '../../../../shared/interfaces/a11y-select';
+import {WcagFilterOptions} from '../../../../shared/enums/wcag-filter';
 
 @Component({
 	selector: 'app-wcag-list-filter',
@@ -7,5 +9,27 @@ import {BaseComponent} from '../../../../core/components/base/base.component';
 	styleUrls: ['./wcag-list-filter.component.scss']
 })
 export class WcagListFilterComponent extends BaseComponent implements OnInit {
-	
+	displayOptions: ISelectOptions = [
+		{
+			label: 'Order by Ids',
+			value: WcagFilterOptions.BY_ID
+		},
+		{
+			label: 'Group By Levels',
+			value: WcagFilterOptions.BY_LEVELS
+		},
+		{
+			label: 'Testing Grouped options',
+			options: [
+				{
+					label: 'Group option1',
+					value: 'test1',
+				},
+				{
+					label: 'Group option 2',
+					value: 'test2',
+				}
+			]
+		}
+	]
 }
--- src/app/shared/components/a11y-select/a11y-select.component.ts
+++ src/app/shared/components/a11y-select/a11y-select.component.ts
@@ -1,16 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, Output, ViewChild} from '@angular/core';
 import {BaseComponent} from '../../../core/components/base/base.component';
+import {ISelectOption, ISelectOptionGroup, ISelectOptions} from '../../interfaces/a11y-select';
 
 @Component({
   selector: 'app-a11y-select',
   templateUrl: './a11y-select.component.html',
   styleUrls: ['./a11y-select.component.scss']
 })
-export class A11ySelectComponent extends BaseComponent implements OnInit {
+export class A11ySelectComponent extends BaseComponent {
 
-  constructor() { }
+	private _selectedOption: ISelectOption;
+	private _options: ISelectOptions = [];
+	private _mappedOptions: { [value: string]: ISelectOption } = {};
 
-  ngOnInit(): void {
-  }
+	@Input() describedByText: string;
+	@Input() describedByIds: string;
+	@Input() label = 'A11y Select';
+	@Input() showDescribedByText = true;
 
+	@Input() set options(value: ISelectOptions) {
+		if (!!value && value.length > 0) {
+			this._options = value;
+			this.generateMappedOptions();
+		}
+	}
+	get options(): ISelectOptions {
+		return this._options;
+	}
+
+	@Input() set selectedOption(value: ISelectOption | undefined) {
+		this._selectedOption = value;
+	}
+	get selectedOption(): ISelectOption {
+		return this._selectedOption;
+	}
+
+	@Output() onOptionSelected: EventEmitter<ISelectOption> = new EventEmitter<ISelectOption>();
+
+	generateDescribedByIds(): string | null {
+		let describedByIds: string | null = null;
+		if (!!this.describedByText) {
+			describedByIds = `a11y-select_describedby_${this.cid} `;
+		}
+		if (!!this.describedByIds) {
+			describedByIds += this.describedByIds;
+		}
+		return describedByIds;
+	}
+
+	private generateMappedOptions(): void {
+		if (this._options && this._options.length > 0) {
+			this._options.forEach((option: ISelectOption | ISelectOptionGroup) => {
+				if (!this.isOptionGroup(option)) {
+					const simpleOption: ISelectOption = option as ISelectOption;
+					this._mappedOptions[simpleOption.value] = simpleOption;
+				} else {
+					const optionGroup: ISelectOptionGroup = option as ISelectOptionGroup;
+					if (!!optionGroup.options && optionGroup.options.length > 0) {
+						optionGroup.options.forEach((groupOption: ISelectOption) => {
+							this._mappedOptions[groupOption.value] = groupOption;
+						});
+					}
+				}
+			});
+		} else {
+			this._mappedOptions = {};
+		}
+	}
+
+	isOptionGroup(option: ISelectOption | ISelectOptionGroup): boolean {
+		return 'options' in option && 'label' in option;
+	}
+
+	onSelectChanged(element: EventTarget): void {
+		const value: string = (element as HTMLSelectElement).value;
+		this.selectedOption = this._mappedOptions[value];
+		console.log(this.selectedOption);
+	}
 }
